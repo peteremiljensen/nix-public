ed8625e81ce3c68fd9db0c45dba45b5c616ccdb8
Peter Emil Jensen <ubs908@gmail.com>
2026-05-20 16:39:33 +0200

    update

--- modules/default.nix
+++ modules/default.nix
@@ -1,13 +1,18 @@
-# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
-# Use `nix run .#write-flake` to regenerate it.
-{
-  outputs = inputs: import ./outputs.nix inputs;
+{inputs, ...}: {
+  imports = [
+    inputs.flake-file.flakeModules.default
+    ./neovim
+  ];
 
-  inputs = {
-    flake-file.url = "github:vic/flake-file";
+  systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+  flake-file.inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-file.url = "github:vic/flake-file";
+
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
     nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
+
     wrappers = {
       url = "github:BirdeeHub/nix-wrapper-modules";
       inputs.nixpkgs.follows = "nixpkgs";
