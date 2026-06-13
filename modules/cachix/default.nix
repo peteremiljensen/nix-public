0dafb5e1d159b36c527fd4fa198cd823a91d4724
Peter Emil Jensen <ubs908@gmail.com>
2026-06-13 09:27:07 +0200

    update

--- modules/cachix/default.nix
+++ modules/cachix/default.nix
@@ -10,5 +10,16 @@
         ];
       }
       (builtins.readFile ./cachix.fish);
+
+    # packages.rust =
+    #   pkgs.writers.writeFishBin "cachix" {
+    #     makeWrapperArgs = [
+    #       "--suffix"
+    #       "PATH"
+    #       ":"
+    #       "${with pkgs; lib.makeBinPath [fish gum cachix jq]}"
+    #     ];
+    #   }
+    #   (builtins.readFile ./cachix.fish);
   };
 }
